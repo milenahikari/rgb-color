b10d27fa1722cd653be4a8b22dbd13e600cdbc51
Milena Hikari <milenahikari11@gmail.com>
2018-12-04 21:37:47 -0200

    first commit

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+//Funcao COR
+function mudarCor(){
+    var r = document.getElementById("r").value;
+	var g = document.getElementById("g").value;
+	var b = document.getElementById("b").value;
+    document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+} 
+
+
+
+
+
